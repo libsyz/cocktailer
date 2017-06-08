5e9a05c37e784277f2a38da7b0e97cce2d167710
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-08 13:38:38 +0200

    routes implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+p "Seeding your booze database"
+
+
+ingredients['drinks'].each do |i|
+      Ingredient.create(name: i.values.join)
+end
+
+
+
