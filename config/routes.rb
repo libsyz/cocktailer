35c1ee9ccdf4c934c483d7e0badd4e90d0e34c42
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-08 21:02:06 +0200

    restarting to understand

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :new, :create, :index, :show] do
-    resources :doses, only: [ :new, :create ]
-  end
-  resources :doses, only: [ :destroy ]
+  resources :cocktails, only: [:index, :show, :new, :create]
 end
