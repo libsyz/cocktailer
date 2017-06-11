0f78d08badb3e87b4c6d69dd0d8413dc85aee3de
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-11 21:09:26 +0200

    ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount Attachinary::Engine => "/attachinary"
-  root 'cocktails#index'
+  root 'cocktails#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
